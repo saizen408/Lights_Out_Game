2c413b03fceca46e6684c5aca98b8b10d12bb5b7
Eric Owusu <llkoole7@gmail.com>
2019-12-04 10:28:23 -0800

    Updates

---
+++ precache-manifest.171d5757dc16d21eea0816b176e1b71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8959b377a5c5728e373f",
+    "url": "/react-lights-out/static/css/main.fef3cb34.chunk.css"
+  },
+  {
+    "revision": "8959b377a5c5728e373f",
+    "url": "/react-lights-out/static/js/main.8959b377.chunk.js"
+  },
+  {
+    "revision": "5faaddd94e967f33f676",
+    "url": "/react-lights-out/static/js/1.5faaddd9.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "c33060898cdde3c0ca9ea1e3e517a9db",
+    "url": "/react-lights-out/index.html"
+  }
+];
